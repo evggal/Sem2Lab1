f91616827133aa6f2c5c0695de82ab7c8dfefc8c
Евгения Галеева <zenagaleeva1315@gmail.com>
2023-02-12 23:33:36 +0300

    Оптимизация программы

--- Lab1/MyFunction.h
+++ Lab1/MyFunction.h
@@ -5,15 +5,10 @@ using namespace std;
 const double dx = 0.00001;
 
 
-vector <double> OpisanieUrav(double a, double b, double c, 
-    double x1, double x2)
+double OpisanieUrav(double a, double b, double c, 
+    double x)
 {
-    vector <double> mas;
-    for (double x = x1; x <= x2; x += dx)
-    {
-        mas.push_back(a * x * x + b * x + c);
-    }
-    return mas;
+    return (a * x * x + b * x + c);
 }
 
 tuple <double, double> Integral(double a1, double b1, double c1,
@@ -53,34 +48,32 @@ tuple <double, double> Integral(double a, double b, double c, bool flag)
     return make_tuple(x1, x2);
 }
 
-double Integral(vector <double> func(double, double, double, double, double), 
+double Integral(double func(double, double, double, double), 
     double a, double b, double c, double x1, double x2)
 {
-    vector <double> dlina;
-    dlina = func(a, b, c, x1, x2);
     double square = 0;
-    for (double i = 1; i < dlina.size(); i++)
+    for (double x = x1+dx; x <= x2; x+=dx)
     {
-        square += dx * (dlina[i] + dlina[i - 1]) / 2;
+        square += dx * (func(a, b, c, x) + func(a, b, c, x-dx)) / 2;
     }
     return square;
 }
 
-double Integral(vector <double> func(double, double, double, double, double),
+double Integral(double func(double, double, double, double),
     double a1, double b1, double c1, double a2, double b2, double c2,
     double x1, double x2)
 {
-    vector <double> y1, y2, dlina;
+    vector <double> dlina;
     double square = 0;
-    y1 = func(a1, b1, c1, x1, x2);
-    y2 = func(a2, b2, c2, x1, x2);
-    for (double i = 0; i < y1.size(); i++)
+    int i = 0;
+    for (double x = x1; x <= x2; x+=dx)
     {
-        dlina.push_back(abs(y1[i] - y2[i]));
+        dlina.push_back(abs(func(a1, b1, c1, x) - func(a2, b2, c2, x)));
         if (i > 0)
         {
             square += dx * (dlina[i] + dlina[i - 1]) / 2;
         }
+        i += 1;
     }
     return square;
 }
